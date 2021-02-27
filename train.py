9ea06791aa18918e669d92a994d8df87461f082b
Peter <peterbon@qq.com>
2021-02-27 08:53:39 +0800

    更新tensorboardx

--- train.py
+++ train.py
@@ -119,7 +119,7 @@ def fit_one_epoch(net, yolo_losses, epoch, epoch_size, epoch_size_val, gen, genv
             pbar.update(1)
 
     # tensorboardX
-    writer.add_scalars('scalar/loss', {'train': total_loss / (epoch_size + 1), 'val': val_loss / (epoch_size_val + 1)},
+    writer.add_scalars('scalars/loss', {'train': total_loss / (epoch_size + 1), 'val': val_loss / (epoch_size_val + 1)},
                        epoch)
     writer.add_scalar('scalar/lr', get_lr(optimizer), epoch)
     writer.flush()
